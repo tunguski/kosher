aa9fdfa45c6d8508f66422ec3a5550fdedb4e4a3
Marek Romanowski <marek.romanowski@gmail.com>
2015-07-04 20:31:16 +0200

    Add more tests

---
+++ src/test/java/pl/matsuo/gitlab/service/TestBuildServiceImpl.java
@@ -0,0 +1,64 @@
+package pl.matsuo.gitlab.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.matsuo.gitlab.hook.BuildInfo;
+import pl.matsuo.gitlab.hook.PushEvent;
+import pl.matsuo.gitlab.hook.Repository;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by marek on 04.07.15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestBuildServiceImpl {
+
+
+  @Mock
+  Database database;
+  @Mock
+  GitRepositoryService gitRepositoryService;
+  @InjectMocks
+  BuildServiceImpl buildService = new BuildServiceImpl();
+
+
+  @Test
+  public void testPushEvent() throws Exception {
+    PushEvent pushEvent = new PushEvent();
+    pushEvent.setRepository(new Repository());
+    pushEvent.setRef("refs/heads/master");
+
+    pushEvent.getRepository().setUrl("git@github.com:tunguski/gitlab-java-event-listener.git");
+    buildService.pushEvent(pushEvent);
+
+    pushEvent.getRepository().setUrl("http://github.com/tunguski/gitlab-java-event-listener.git");
+    buildService.pushEvent(pushEvent);
+  }
+
+
+  @Test
+  public void testBuildStatus() throws Exception {
+    PushEvent pushEvent = new PushEvent();
+    pushEvent.setRepository(new Repository());
+    pushEvent.setRef("refs/heads/master");
+    pushEvent.getRepository().setUrl("git@github.com:tunguski/gitlab-java-event-listener.git");
+
+    String idBuild = buildService.pushEvent(pushEvent);
+
+    BuildInfo result = new BuildInfo();
+    result.setId(idBuild);
+
+    Mockito.when(database.get(idBuild, BuildInfo.class)).thenReturn(result);
+
+    BuildInfo buildInfo = buildService.buildStatus(idBuild);
+
+    assertEquals(idBuild, buildInfo.getId());
+  }
+}
+
---
+++ src/test/java/pl/matsuo/gitlab/service/TestMapDbDatabase.java
@@ -0,0 +1,52 @@
+package pl.matsuo.gitlab.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by marek on 04.07.15.
+ */
+public class TestMapDbDatabase {
+
+
+  MapDbDatabase database = new MapDbDatabase();
+
+
+  @Test
+  public void testPutAndGet() throws Exception {
+    database.put("test", "value");
+    assertEquals("value", database.get("test", String.class));
+  }
+
+
+  @Test
+  public void testKeySet() throws Exception {
+    database.put("test", "value");
+    assertEquals(1, database.keySet().size());
+  }
+
+
+  @Test
+  public void testValues() throws Exception {
+    database.put("test", "value");
+    assertEquals(1, database.values().size());
+  }
+
+
+  @Test
+  public void testSize() throws Exception {
+    database.put("test", "value");
+    assertEquals(1, database.size());
+  }
+
+
+  @Test
+  public void testIsEmpty() throws Exception {
+    assertTrue(database.isEmpty());
+    database.put("test", "value");
+    assertFalse(database.isEmpty());
+  }
+}
+
